58f6e5df4a82528c73b72d942cd5e68ade151a51
Anna Egorova <anna-e-gorova@yandex.ru>
2021-05-30 14:53:21 +0300

    HW1_done

--- script.js
+++ script.js
@@ -5,22 +5,19 @@ const goods = [
   { title: 'Shoes', price: 250 },
 ]
 
-const renderGoodsItem = (title, price) => {
-  return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`
-}
+const renderGoodsItem = (title = 'Empty', price = 0) => `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>` 
 
-const renderGoodsList = list => {
-  let goodsList = list.map(item => renderGoodsItem(item.title, item.price))
-
-  //   const goodsListDiv = document.querySelector('.goods-list')
-  //   goodsListDiv.innerHTML = goodsList
-
-  document.querySelector('.goods-list').innerHTML = goodsList
-
-}
+const renderGoodsList = (list = goods) => list.map(item => document.querySelector('.goods-list').insertAdjacentHTML('beforeend', renderGoodsItem(item.title, item.price)));
 
 const init = () => {
-  renderGoodsList(goods)
-}
+  renderGoodsList(goods);
+} 
 
 window.onload = init
+ 
+/*
+2. Добавьте значения по умолчанию для аргументов функции. Как можно упростить или сократить запись функций?
+Ответ: значения по умолчанию доюавлены. Чтобы упростить запись можно убрать фигурные скобки и команду return:
+3. Сейчас после каждого товара на странице выводится запятая. Из-за чего это происходит? Как это исправить?
+Ответ: в результате list.map создается массив с запятыми, чтобы этого избежать на каждом шаге добавляем разметку.
+*/
