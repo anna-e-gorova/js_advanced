fc5d4e199cc54eb17545a83bd420fc422061c504
Anna Egorova <anna-e-gorova@yandex.ru>
2021-06-03 18:57:14 +0300

    hw2

---
+++ hw2/script.js
@@ -0,0 +1,70 @@
+class GoodsItem {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+  }
+}
+
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+
+  fetchGoods() {
+    this.goods = [
+      { title: "Shirt", price: 150 },
+      { title: "Socks", price: 50 },
+      { title: "Jacket", price: 350 },
+      { title: "Shoes", price: 250 },
+    ];
+  }
+
+  render() {
+    let listHtml = "";
+    this.goods.forEach((good) => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector(".goods-list").innerHTML = listHtml;
+  }
+
+  summ() {
+    let listSumm = 0;
+    this.goods.forEach((good) => {
+      listSumm += good.price;
+    });
+    return listSumm;
+  }
+}
+
+class BasketItem extends GoodsItem {
+  constructor(){
+    this.quantity = 1;
+  }
+  increase() {
+    this.quantity++;
+  }
+  decrease(){
+    if (this.quantity > 1){
+      this.quantity--;
+    }
+  }
+}
+
+class Basket extends GoodsList {
+  addendum() {}
+  removal() {}
+
+}
+
+const init = () => {
+    const list = new GoodsList();
+    list.fetchGoods();
+    list.render();
+    console.log(list.summ());
+};
+
+window.onload = init;
